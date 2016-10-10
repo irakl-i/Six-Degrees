c8eb32a69cd78d2ea0c73efa94dfa377ac08a1a8
Irakli <i@irak.li>
2016-10-11 00:48:20 +0400

    Add binary search for actors

--- imdb.cc
+++ imdb.cc
@@ -11,6 +11,11 @@ using namespace std;
 const char *const imdb::kActorFileName = "actordata";
 const char *const imdb::kMovieFileName = "moviedata";
 
+struct key {
+	const char* str;
+	const void* file;
+};
+
 imdb::imdb(const string& directory)
 {
 	const string actorFileName = directory + "/" + kActorFileName;
@@ -26,21 +31,39 @@ bool imdb::good() const
 			(movieInfo.fd == -1) ); 
 }
 
-int cmpActors(const void * a, const void * b) {
-	// კომპარატორი დავწეროთ
+int cmpActors(const void * a, const void * b)
+{
+	key local = *(key*)a;
+	string s1((char*)local.file + *(int*)b);
+	string s2(local.str);
+	cout << s1 << " =? " << s2 << endl;	
+	if(s2 > s1) return 1;
+	if(s2 < s1) return -1;
+	return 0;
 }
 
-// you should be implementing these two methods right here... 
 bool imdb::getCredits(const string& player, vector<film>& films) const 
 {
 	size_t num = *(int*)actorFile;
 	//cout << actorNumber << endl;
 	
-	const char* p = player.c_str();
+	// for (size_t i = 1; i <= num; i++)
+	// {
+	// 	int temp = *(int*)((char*)actorFile + i*sizeof(int));	
+	// 	string s((char*)actorFile + temp);
+	// 	cout << s << endl;
+	// 	if (s == "999") break;		
+	// }
+	//int* i = (int*)(char*)actorFile + sizeof(int);
+	
+	key k;
+	k.str = player.c_str();
+	k.file = actorFile;
 	//printf("%s\n", p);
-	int* playerLocation = (int*)bsearch(&p, (char*)actorFile + sizeof(int), num, sizeof(int), cmpActors);
+	int* playerLocation = (int*)bsearch(&k, (char*)actorFile + sizeof(int), num, sizeof(int), cmpActors);
 	return false;
 }
+
 bool imdb::getCast(const film& movie, vector<string>& players) const
 { 
 	return false; 
